f894e6d95512a8f0292602fee7279bbca69e24e3
Laeken Rosado <laeken.rosado@gmail.com>
2022-07-08 18:03:21 +0200

    Updating Exercises

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const { response } = require("express");
+const lodash = require('lodash');
 // server.js
 // This is where your node app starts
 
@@ -14,10 +15,46 @@ const quotes = require("./quotes.json");
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+  response.send("Laeken's Quote Server!  Ask me for /quotes/random, or /quotes");
 });
 
 //START OF YOUR CODE...
+
+// --Mostrar todas las quotes.
+app.get("/quotes", function (req, res) {
+  res.send(quotes);
+});
+
+// --Mostrar una quote al azar.
+app.get("/quotes/random", function (req, res) {
+  const randomQuote = pickFromArray(quotes);
+  res.send(randomQuote);
+});
+
+// --Filtrar quotes.
+app.get("/quotes/search", function (req, res) {
+  const searchTerm = req.query.term.toLowerCase();
+  const searchQuotes = quotes.filter((quoteObj) => {
+    
+    // --Encontrar Quotes que contengan Xs palabra
+    const quoteByWordToLowerCase = quoteObj.quote.toLowerCase();
+    const findQuotebyWord = quoteByWordToLowerCase.includes(searchTerm);
+    
+    // --Encontrar Quotes con el mismo Author
+    const quoteByAuthorToLowerCase = quoteObj.author.toLowerCase();
+    const findQuotebyAuthor = quoteByAuthorToLowerCase.includes(searchTerm);
+
+    return findQuotebyWord || findQuotebyAuthor ;
+  });
+
+  res.send(searchQuotes);
+});
+
+// --Obtener una quote aleatoria mediante lodash.
+app.get("/lodash", function (request, response) {
+  response.send(lodash.sample(quotes));
+});
+
 
 //...END OF YOUR CODE
 
@@ -32,6 +69,6 @@ function pickFromArray(arr) {
 //Start our server so that it listens for HTTP requests!
 let port = 5000;
 
-app.listen( port, function () {
+app.listen(port, function () {
   console.log("Your app is listening on port " + port);
 });
